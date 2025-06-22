b3170589896650ee0fc0ab2cd04c00c126e8930c
Deepesh Gupta <learning.is.first.earning@gmail.com>
2025-06-22 07:25:48 +0530

    add function to extract keywords from text

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import { extractAllKeywordFromText } from "./services/keywords.js";
+import { extractRepeatedKeywordFromText } from "./services/keywords.js";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     return res.send('Welcome to pdf parser');
 });
+
+app.post('/all', async (req, res) => {
+    const text = req.body.text;
+    const keyWords = extractAllKeywordFromText(text);
+    return res.json({ keyWords: keyWords });
+})
+
+app.post('/repeated', async (req, res) => {
+    const text = req.body.text;
+    const keyWords = await extractRepeatedKeywordFromText(text);
+    return res.json({ keyWords: keyWords });
+})
 
 app.listen(port, (err) => {
     console.log(`server is listening to port ${port}`);
---
+++ server/services/keywords.js
@@ -0,0 +1,37 @@
+import keyword_extractor from "keyword-extractor";
+
+export const extractAllKeywordFromText = (text) => {
+    const keyWords = keyword_extractor.extract(text, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: true,
+    });
+
+    return keyWords;
+}
+
+import { retext } from "retext";
+import retextKeywords from "retext-keywords";
+import retextPos from "retext-pos";
+import { toString } from "nlcst-to-string"
+
+export const extractRepeatedKeywordFromText = async (text) => {
+    const file = await retext()
+        .use(retextPos)
+        .use(retextKeywords)
+        .process(text);
+
+    if (file.data.keywords) {
+        
+        const keywords = file.data.keywords.map((keyword) => {
+            for (const matche of keyword.matches) {
+                console.log(keyword.matches.indexOf(matche), matche.node.children[0].value);
+            }
+            return toString(keyword.matches[0].node);
+        });
+        return keywords;
+    }
+    return [];
+}
+
